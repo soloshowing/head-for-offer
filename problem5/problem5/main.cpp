2f29d37a9824a97a62e7b6a9b43e177ae838a101
soloshowing <978949248@qq.com>
2019-04-07 14:00:36 +0800

    剑指offer 第五题：替换空格

---
+++ problem5/problem5/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+void replace_blank(char* s) {
+	char *p = s;
+	char* q = s;
+
+	while (*p != '\0')
+		q += (*p++ != ' ') ? 1 : 3;
+	
+	while (p != q)
+	{
+		if (*p != ' ')
+			*q-- = *p--;
+		else
+		{
+			*q-- = '0';
+			*q-- = '2';
+			*q-- = '%';
+			p--;
+		}
+	}
+}
+
+int main()
+{
+	char str[100] = "are you okay? hello thank you, thank you very much";
+	replace_blank(str);
+	cout << str << endl;
+	return 0;
+}
