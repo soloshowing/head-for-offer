831ecea974747047da402ab63586d237016dc155
soloshowing <978949248@qq.com>
2019-04-10 20:32:23 +0800

    面试题20：表示数值的字符串

---
+++ problem20/problem20/isNumeric.cpp
@@ -0,0 +1,33 @@
+﻿bool isNumeric(char* str)
+{
+	bool sign = false, decimal = false, hasE = false;
+
+	for (char* s = str; *s != '\0'; ++s)
+	{
+		if (*s == 'e' || *s == 'E')
+		{
+			//e只能出现一次，且e之后必须有数字
+			if (hasE || *s == '\0')
+				return false;
+			hasE = true;
+		}
+		else if (*s == '+' || *s == '-')
+		{
+			//正负号只能在开头或者e之后出现，别无其它可能
+			if (s == str)
+				sign = true;
+			else if (*(s - 1) != 'e' && *(s - 1) != 'E')
+				return false;
+		}
+		else if (*s == '.')
+		{
+			//只能有一个小数点且e之后不能有小数点
+			if (hasE || decimal)
+				return false;
+			decimal = true;
+		}
+		else if (*s <'0' || *s >'9')
+			return false;
+	}
+	return true;
+}
