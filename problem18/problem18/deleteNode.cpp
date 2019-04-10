40d3440f9fed35e5c9bdf627475dabf80d4893d9
soloshowing <978949248@qq.com>
2019-04-10 10:11:10 +0800

    面试题18：第一题：删除链表的节点

---
+++ problem18/problem18/deleteNode.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int value;
+	Node* next;
+};
+
+//无法保证被删除节点是否在链中
+void delNode(Node* pRoot, Node* pDeleted)
+{
+	if (!pRoot || !pDeleted)
+		return;
+	//不是尾节点
+	if (pDeleted->next)
+	{
+		Node* pNext = pDeleted->next;
+		pDeleted->value = pNext->value;
+		pDeleted->next = pNext->next;
+		delete pNext;
+		pNext = nullptr;
+	}
+	//链只有一个节点
+	else if (pRoot == pDeleted)
+	{
+		delete pRoot;
+		pRoot = pDeleted = nullptr;
+	}
+	//普通的尾节点
+	else
+	{
+		Node* pNode = pRoot;
+		while (pNode->next != pDeleted)
+		{
+			pNode = pNode->next;
+		}
+		pNode->next = nullptr;
+		delete pDeleted;
+		pDeleted = nullptr;
+	}
+
+}
+
+int main()
+{
+	return 0;
+}
