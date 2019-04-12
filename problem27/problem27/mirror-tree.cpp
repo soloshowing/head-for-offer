17832042aff13f41e694ce75c38daadd0cc1528f
soloshowing <978949248@qq.com>
2019-04-12 10:55:29 +0800

    面试题27：二叉树的镜像 添加两个非递归方法：分别使用队列和栈

--- problem27/problem27/mirror-tree.cpp
+++ problem27/problem27/mirror-tree.cpp
@@ -1,3 +1,5 @@
+#include <queue>
+#include <stack>
 struct TreeNode {
 	int val;
 	struct TreeNode *left;
@@ -9,7 +11,7 @@ struct TreeNode {
 
 class Solution {
 public:
-	void Mirror(TreeNode *root) {
+	void Mirror(TreeNode* root) {
 		if (root == nullptr)
 			return;
 		TreeNode* tmp = root->left;
@@ -18,5 +20,43 @@ public:
 		Mirror(root->left);
 		Mirror(root->right);
 		return;
+	}
+
+	void MirrorQueue(TreeNode* root)
+	{
+		if (root == nullptr)
+			return;
+		std::queue<TreeNode*> q;
+		q.push(root);
+		while (q.empty() == false)
+		{
+			auto cur = q.front();
+			auto tmp = cur->left;
+			cur->left = cur->right;
+			cur->right = tmp;
+			q.pop();
+			if(cur->left)
+				q.push(cur->left);
+			if(cur->right)
+				q.push(cur->right);
+		}
+	}
+
+	void MirrorStack(TreeNode* root)
+	{
+		if (root == nullptr)
+			return;
+		std::stack<TreeNode*> stk;
+		stk.push(root);
+		while (stk.empty() == false)
+		{
+			auto cur = stk.top();
+			stk.pop();
+			if(cur->left)
+				stk.push(cur->left);
+			if (cur->right)
+				stk.push(cur->right);
+			std::swap(cur->left, cur->right);
+		}
 	}
 };
