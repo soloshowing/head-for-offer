3c96d08b5763bb8aebd6379db56adcc72db1679d
soloshowing <978949248@qq.com>
2019-04-13 17:04:30 +0800

    面试题35：复杂链表的复制

---
+++ problem35/problem35/main.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+struct RandomListNode {
+	int label;
+	struct RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+class Solution {
+public:
+	//无脑的方法
+	RandomListNode* CloneFool(RandomListNode* pHead)
+	{
+		if (pHead == nullptr)
+			return nullptr;
+		auto newHead = new RandomListNode(pHead->label);
+		auto pNew = newHead;
+		auto pOld = pHead;
+		while (pOld->next)
+		{
+			pNew = pNew->next = new RandomListNode(pOld->next->label);
+			pOld = pOld->next;
+		}
+
+		pNew = newHead;
+		while (pHead)
+		{
+			if (pHead->random)
+			{
+				auto p = newHead;
+				int value = pHead->random->label;
+				while (p)
+				{
+					if (p->label == value)
+						pNew->random = p;
+					p = p->next;
+				}
+			}
+			pHead = pHead->next;
+			pNew = pNew->next;
+		}
+		return newHead;
+	}
+
+	RandomListNode* CloneSmart(RandomListNode* pHead)
+	{
+		if (pHead == nullptr)
+			return nullptr;
+		auto curNode = pHead;
+		while (curNode) {
+			auto newNode = new RandomListNode(curNode->label);
+			newNode->next = curNode->next;
+			curNode->next = newNode;
+			curNode = newNode->next;
+		}
+
+		curNode = pHead;
+		while (curNode)
+		{
+			if (curNode->random)
+				curNode->next->random = curNode->random->next;
+			curNode = curNode->next->next;
+		}
+		curNode = pHead;
+		auto newHead = pHead->next;
+		while (curNode->next)
+		{
+			auto temp = curNode->next;
+			curNode->next = temp->next;
+			curNode = temp;
+		}
+		return newHead;
+	}
+};
