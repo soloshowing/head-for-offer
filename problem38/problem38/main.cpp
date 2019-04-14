b2e3db76aaa9775be56ada52f03a1b5189300d54
soloshowing <978949248@qq.com>
2019-04-14 15:45:19 +0800

    面试题38：字符串的排列

---
+++ problem38/problem38/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+//回溯法
+class Solution {
+public:
+	vector<string> Permutation(string str) {
+		vector<string> result;
+		Permutation(str, 0, result);
+		sort(result.begin(), result.end());
+		return result;
+	}
+
+	void Permutation(string& s, int left, vector<string>& result)
+	{
+		if (left == s.length() - 1)
+		{
+			result.push_back(s);
+			return;
+		}
+		for (int i = left; i < s.length(); i++)
+		{
+			if (i != left && s[i] == s[left])
+				continue;
+			swap(s[left], s[i]);
+			Permutation(s, left + 1, result);
+			swap(s[i], s[left]);
+		}
+	}
+};
+
+int main()
+{
+	string str = "abc";
+	Solution s;
+	auto result = s.Permutation(str);
+
+	for (const string& s : result)
+		cout << s << endl;
+	return 0;
+}
