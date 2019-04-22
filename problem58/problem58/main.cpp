4d5f966e33b29c246b20461bf6d028e099ab03b4
soloshowing <978949248@qq.com>
2019-04-22 15:09:01 +0800

    面试题58：面试题1：翻转字符串

---
+++ problem58/problem58/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	string ReverseSentence(string str) {
+		Reverse(str, 0, str.size() - 1);
+		int s = 0, e = 0;
+		int i = 0;
+		while (i < str.size())
+		{
+			while (i < str.size() && str[i] == ' ')
+				i++;
+			e = s = i;//记录单词的第一个单词的位置
+			while (i < str.size() && str[i] != ' ')
+			{
+				i++;
+				e++;
+			}
+			Reverse(str, s, e - 1);
+		}
+		return str;
+	}
+
+	void Reverse(string& str, int b, int e)
+	{
+		if (b < e)
+			swap(str[b++], str[e--]);
+	}
+};
