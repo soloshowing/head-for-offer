510a2c80ecb103973ae1d5085cd7a0ff2be631c4
soloshowing <978949248@qq.com>
2019-04-23 10:31:27 +0800

    面试题43：1~n整数中1出现的次数

---
+++ problem43/problem43/main.cpp
@@ -0,0 +1,25 @@
+﻿class Solution {
+public:
+	int NumberOf1Between1AndN_Solution(int n)
+	{
+		int count = 0;//1的个数
+		int i = 1;//当前位
+		int current = 0, after = 0, before = 0;
+		while (n / i)
+		{
+			current = (n / i) % 10;//高位数字
+			before = n / (i * 10);//当前位数字
+			after = n - (n / i)*i;//低位数字
+
+			if (current == 0)
+				count += before * i;
+			else if (current == 1)
+				count += before * i + after + 1;
+			else
+				count += (before + 1)*i;
+
+			i = i * 10;
+		}
+		return count;
+	}
+};
