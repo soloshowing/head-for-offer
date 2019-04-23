4744d45e96fe62d5ff4c403489ee7ec59118ec4b
soloshowing <978949248@qq.com>
2019-04-23 18:58:07 +0800

    面试题51：数组中的逆序对

---
+++ problem51/problem51/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <vector>
+using namespace std;
+
+long getCount(vector<int>& data, vector<int> &copy, long start, long end)
+{
+	if (start == end)
+		return 0;
+	long mid = start + ((end - start) >> 1);
+	long lCount = getCount(copy, data, start, mid);
+	long rCount = getCount(copy, data, mid + 1, end);
+	long crossCount = 0; //记录交叉的逆序对数
+	long i = mid, j = end, tempIdx = end;
+
+	while (i >= start && j > mid)
+	{
+		if (data[i] > data[j])
+		{
+			copy[tempIdx--] = data[i--];
+			crossCount += j - start - (mid - start);
+		}
+		else
+		{
+			copy[tempIdx--] = data[j--];
+		}
+	}
+
+	while (i >= start)
+		copy[tempIdx--] = data[i--];
+	while (j > mid)
+		copy[tempIdx--] = data[j--];
+	return (lCount + rCount + crossCount) % 1000000007;
+}
+
+int InversePairs(vector<int>& data)
+{
+	if (data.size() == 0)
+		return 0;
+	else if (data.size() == 1)
+		return 1;
+	else
+	{
+		vector<int> copy(data);
+		return getCount(data, copy, 0, data.size() - 1);
+	}
+}
